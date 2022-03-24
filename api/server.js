6d9f5ef8838000b43bccadc509058e3bd329b10e
chethanck <chethanckm66@gmail.com>
2022-03-24 20:57:15 +0530

    Adding routes

--- api/server.js
+++ api/server.js
@@ -3,24 +3,16 @@ const dotenv = require('dotenv')
 const { chats } = require('./data/data.js')
 const connectDB = require('./config/db.js')
 const colors = require('colors')
-
+const userRoutes = require('./routes/userRoutes.js')
 const app = express()
 dotenv.config()
 connectDB()
 
 app.get('/', (req, res) => {
-  res.send('API IS RUNNINg')
+  res.send('API IS RUNNING')
 })
 
-app.get('/api/chat', (req, res) => {
-  res.send(chats)
-})
-
-app.get('/api/chat/:id', (req, res) => {
-  //console.log(req.params.id)
-  const singleChat = chats.find((c) => c._id === req.params.id)
-  res.send(singleChat)
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT
 
