c0c1a5549467d79b9e84be4eb5894481cb90179a
chethanck <chethanckm66@gmail.com>
2022-03-24 20:46:07 +0530

    Mongo DB Connection

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const { chats } = require('./data/data.js')
+const connectDB = require('./config/db.js')
+const colors = require('colors')
 
 const app = express()
 dotenv.config()
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('API IS RUNNINg')
@@ -22,5 +25,5 @@ app.get('/api/chat/:id', (req, res) => {
 const PORT = process.env.PORT
 
 app.listen(8000, () => {
-  console.log(`Server Listenining on port ${PORT}`)
+  console.log(`Server Listenining on port ${PORT}`.yellow.bold)
 })
