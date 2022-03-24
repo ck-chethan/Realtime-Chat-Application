f2f73b7a0a9ff33044c634931977aab379bb6439
chethanck <chethanckm66@gmail.com>
2022-03-24 22:08:46 +0530

    Handling routing errors

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data.js')
 const connectDB = require('./config/db.js')
 const colors = require('colors')
 const userRoutes = require('./routes/userRoutes.js')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 
 dotenv.config()
 connectDB()
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/user', userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
