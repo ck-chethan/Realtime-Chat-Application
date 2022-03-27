b75bd9359578db0461cd9a47d5e1d33d4426f475
chethanck <chethanckm66@gmail.com>
2022-03-27 14:56:13 +0530

    Adding controllers to fetch all the chats

--- api/controllers/chatControllers.js
+++ api/controllers/chatControllers.js
@@ -26,7 +26,6 @@ const accessChat = asyncHandler(async (req, res) => {
 
   if (isChat.length > 0) {
     res.send(isChat[0])
-    console.log(req)
   } else {
     var chaData = {
       chatName: 'sender',
@@ -48,4 +47,24 @@ const accessChat = asyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { accessChat }
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+      .populate('latestMessage')
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: 'latestMessage.sender',
+          select: 'name pic email',
+        })
+        res.status(200).send(results)
+      })
+  } catch (error) {
+    res.status(400)
+    throw new Error(error.message)
+  }
+})
+
+module.exports = { accessChat, fetchChats }
