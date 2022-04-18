4b9bf99b1be26e60646acc7c5dba223e9935c558
chethanck <chethanckm66@gmail.com>
2022-04-18 13:56:29 +0530

    Chatbox and Mychats layout starting files

--- client/src/pages/chatPage/ChatPage.js
+++ client/src/pages/chatPage/ChatPage.js
@@ -2,15 +2,23 @@ import { Box } from '@chakra-ui/react'
 import SideDrawer from '../../components/miscellaneous/sideDrawer/SideDrawer'
 import { ChatState } from '../../context/ChatProvider'
 import '../chatPage/chatPage.css'
+import ChatBox from '../../components/chatBox/ChatBox'
+import MyChats from '../../components/myChats/MyChats'
 
 const ChatPage = () => {
   const { user } = ChatState()
   return (
     <div style={{ width: '100%' }}>
       {user && <SideDrawer />}
-      <Box>
-        {/* {user && <MyChats/>} */}
-        {/* {user && <ChatBox/>} */}
+      <Box
+        d="flex"
+        justifyContent={'space-between'}
+        w="100%"
+        h="91.5vh"
+        p={'10px'}
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
       </Box>
     </div>
   )
